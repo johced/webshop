9b79d5af05733121f707a381a807927a1fe8d7ad
johced <johan.cederstrand@bahnhof.se>
2020-12-04 08:50:59 +0100

    Added our objects/products in main.js. Modified grid main area. Added code in productslist.js and made changes to productslist.html, and added pictures of objects in imagesfolder

--- js/productslist.js
+++ js/productslist.js
@@ -1,1 +1,13 @@
-$(function () {});
+$(function () {
+	$.each(productsList, (i, book) => {
+		$('<div>')
+			.attr('id', 'productlistBox')
+			.html('<img src="' + book.poster + '">' + '<h3>' + book.title + '</h3>' + '<p>' + book.price + '</p>' + '<button type="button" alt="button">' + 'Läs mer!' + '</button>')
+			.on('click', { book: book }, handleBookClick)
+			.appendTo($('#main-area'));
+	});
+});
+
+function handleBookClick(e) {
+	console.log(e.data.book);
+}
