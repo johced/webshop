c973df8b5ea6e4f989602af2d8a4d599a360e769
johced <johan.cederstrand@bahnhof.se>
2020-12-07 08:42:14 +0100

    created the function to show object from sessionStorage, change text to buy on button and made button clickable

--- js/productdetailpage.js
+++ js/productdetailpage.js
@@ -1,1 +1,30 @@
-$(function () {});
+$(function () {
+	let productDetailObjectAsTextFromLS = sessionStorage.getItem('productDetailObject');
+	let productDetailObjectFromLS = JSON.parse(productDetailObjectAsTextFromLS);
+	$.each(productDetailObjectFromLS, (i, book) => {
+		$('<div>')
+			.attr('id', 'productlistBoxDetail')
+			.html(
+				'<div id="productLeft"><img src="' +
+					book.poster +
+					'">' +
+					'<h3>' +
+					book.title +
+					'</h3>' +
+					'<p>' +
+					book.price +
+					'</p>' +
+					'<button type="button" id="buyButton" alt="button">' +
+					'Köp' +
+					'</button></div>' +
+					'<div id="productDescription"><h5>' +
+					book.description +
+					'</h5></div>'
+			)
+			.appendTo($('#main-area'));
+
+		$('#buyButton').on('click', { book: book }, () => {
+			console.log(book);
+		});
+	});
+});
