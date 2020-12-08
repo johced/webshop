833a74c8cdf565d7ebe254189fdaa87e8cc54650
Sumi Kim <ksm2920@gmail.com>
2020-12-08 10:26:00 +0100

    The shopping-cart icon is now placed at beside of hamburger menu bar in mobile

--- js/nav.js
+++ js/nav.js
@@ -2,37 +2,39 @@ $(function () {
     let logoDiv = $('#main-logo');
     $('<a>').attr('href','../index.html').attr('id','books-logo').html('BOOKS.').appendTo(logoDiv);
     
-    let mainNav = $('#main-nav');
-    let navTag = $('<nav>').addClass('active').attr('id', 'desktop-only').appendTo(mainNav);
+    // let mainNav = $('#main-nav');
+    // let navTag = $('<nav>').addClass('active').attr('id', 'desktop-only').appendTo(mainNav);
     
-    let ulTag = $('<ul>').appendTo(navTag);
-    let aboutLink = $('<li>').appendTo(ulTag);
-    let productLink = $('<li>').appendTo(ulTag);
-    let contactLink = $('<li>').appendTo(ulTag);
-    let cartLink = $('<li>').appendTo(ulTag);
+    // let ulTag = $('<ul>').appendTo(navTag);
+    // let aboutLink = $('<li>').appendTo(ulTag);
+    // let productLink = $('<li>').appendTo(ulTag);
+    // let contactLink = $('<li>').appendTo(ulTag);
+    // let cartLink = $('<li>').appendTo(ulTag);
     
-    $('<a>').attr('href', '#').html('About Us').appendTo(aboutLink); 
-    $('<a>').attr('href', '../html/productslist.html').html('Products').appendTo(productLink); 
-    $('<a>').attr('href', '#').html('Contact Us').appendTo(contactLink); 
-    let cartAnchor = $('<a>').attr('href', '../html/checkout.html').appendTo(cartLink)
-    $('<i>').addClass('fas fa-shopping-basket').attr('id', 'shopping-cart').appendTo(cartAnchor); 
+    // $('<a>').attr('href', '#').html('About Us').appendTo(aboutLink); 
+    // $('<a>').attr('href', '../html/productslist.html').html('Products').appendTo(productLink); 
+    // $('<a>').attr('href', '#').html('Contact Us').appendTo(contactLink); 
+    // let cartAnchor = $('<a>').attr('href', '../html/checkout.html').appendTo(cartLink)
+    // $('<i>').addClass('fas fa-shopping-basket').attr('id', 'shopping-cart').appendTo(cartAnchor); 
 
-    let divTag = $('<div>').addClass('menu-toggle').appendTo(mainNav);
-    $('<i>').addClass('fas fa-bars').appendTo(divTag);
+    // let divTag = $('<div>').addClass('menu-toggle').appendTo(mainNav);
+    // $('<i>').addClass('fas fa-bars').appendTo(divTag);
     
-    // let navDivHtml = `<nav class="active" id="desktop-only">
-    // <ul>
-    // <li><a href="#">About Us</a></li>
-    // <li><a href="html/productslist.html">Products</a></li>
-    // <li><a href="#">Contact Us</a></li>
-    // <li><a href="#"><i class="fas fa-shopping-basket" id="shopping-cart"></i></a></li>
-    // </ul>
-    // </nav>
-    // <div class="menu-toggle">
-    // <i class="fas fa-bars"></i>
-    // </div>`;
+    let navDivHtml = `<nav class="active" id="desktop-only">
+    <ul>
+    <li><a href="#">About Us</a></li>
+    <li><a href="../html/productslist.html">Products</a></li>
+    <li><a href="#">Contact Us</a></li>
+    </ul>
+    </nav>
+    <div id="cart" class="cart" deta-totalitems="0">
+    <a href="../html/checkout.html"><i class="fas fa-shopping-basket" id="shopping-cart"></i></a>
+    </div>
+    <div class="menu-toggle">
+    <i class="fas fa-bars"></i>
+    </div>`;
     
-    // $("#main-nav").html(navDivHtml);
+    $("#main-nav").html(navDivHtml);
     
     $('.menu-toggle').on('click', function(){
         $('nav').toggleClass('active');
