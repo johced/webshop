f6afec23b9bb104a25514e71eb4f0bf1f7ebadda
Sumi Kim <ksm2920@gmail.com>
2020-12-14 13:56:56 +0100

    A contact us page is added to website

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,37 @@
-$(function () {});
+$(function () {
+    let contactDivHtml = `
+    <div id="contact-header">
+    <h1>Contact Us</h1>
+    </div>
+    <div id="address" class="contact">
+    <i class="fas fa-map-marker-alt contact-icons"></i>
+    <h3 class="contact-method">
+    ADRESS
+    </h3>
+    <p class="contact-text"><b>BOOKS.</b></br>
+    Gustavslundsvägen 151 D</br>
+    167 51 Bromma
+    </p>
+    </div>
+    <div id="phone" class="contact">
+    <i class="fas fa-mobile-alt contact-icons"></i>
+    <h3 class="contact-method">
+    PHONE
+    </h3>
+    <p class="contact-text">08-123-456-78<br/>
+    Operating Hours:<br/>
+    <b>Mon - Fri:</b> 9am - 6pm</p>
+    </div>
+    <div id="email" class="contact">
+    <i class="fas fa-envelope-open-text contact-icons"></i>
+    <h3 class="contact-method">
+    EMAIL
+    </h3>
+    <p class="contact-text"><a href="#">Click here</a> to email<br/>
+    BOOKS. customer service<br/>
+    <a href="#">info@books.se</a></p>
+    </div>`
+    
+    $('#main-area').html(contactDivHtml);
+    
+});
