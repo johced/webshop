09effb1f25b846a79a93b76f3ee4829636e3f6db
Sumi Kim <ksm2920@gmail.com>
2020-12-15 15:58:20 +0100

    A validate function is added to checkout form

--- js/payment.js
+++ js/payment.js
@@ -1,102 +1,7 @@
 $(function () {
-    let checkoutHtml = `<div class="checkout">
-    <div class="cart-section">
-    <h3 id="header">1. Shopping cart</h3>
-    <ol id="added-products"></ol>
-    <div class="total-amount">
-    <p>Total: </p>
-    <p id="total-amount"></p>
-    </div>
-    <button class="continue-1">Continue</button>
-    </div>
-    
-    <div id="information-section" class="checkout-section">
-    <h3 id="header">2.Shipping address</h3>
-    <label for="name"> Full Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Kalle Anka">
-    <label for="email"> Email</label>
-    <input type="text" id="email" name="email" placeholder="kalleAnka@example.com">
-    <label for="address"> Address</label>
-    <input type="text" id="address" name="address" placeholder="Gustavslundsvägen 151 D">
-    <label for="city"> City</label>
-    <input type="text" id="city" name="city" placeholder="Bromma">
-    <label for="zip">Zip</label>
-    <input type="text" id="zip" name="zip" placeholder="10001">
-    <label for="country">Country</label>
-    <select id="country" name="country">
-    <option value="Sweden">Sweden</option>
-    </select>
-    </div>
-    
-    <div id="delivery-section" class="checkout-section">
-    <h3 id="header">3.Shipping Method</h3>
-    <p>Delivery</p>
-    <div>
-    <input type="radio" id="standard" name="deliveryType" value="Standard delivery 4-7 Bussiness Days">
-    <label for="standard">Standard 4-7 Bussiness Days - Free </label><br>
-    <input type="radio" id="express" name="deliveryType" value="Express delivery 2-3 Bussiness Days">
-    <label for="express">Express 2-3 Bussiness Days - Free </label><br>
-    </div>
-    <button class="continue-2">Continue</button>
-    </div>
-    
-    <div id="payment-section" class="checkout-section">  
-    <h3 id="header">4.Payment Details</h3>
-    <div>
-    <input type="radio" id="card" name="card" value="Credit Card">
-    <label for="card">Credit Card</label>
-    </div>
-    <div class="icon-container">
-    <i class="fa fa-cc-visa"></i>
-    <i class="fa fa-cc-mastercard"></i>
-    </div>
-    <label for="cardname">Name on Card</label>
-    <input type="text" id="cardname" name="cardname" placeholder="Kalle Anka">
-    <label for="cardnum">Credit card number</label>
-    <input type="text" id="cardnum" name="cardnumber" placeholder="****-****-****-****">
-    <label for="expmonth">Exp Month</label>
-    <input type="text" id="expmonth" name="expmonth" placeholder="December">
-    <label for="expyear">Exp Year</label>
-    <input type="text" id="expyear" name="expyear" placeholder="2020">
-    <label for="cvc">CVC</label>
-    <input type="text" id="cvc" name="cvc" placeholder="***">
-    <button class="continue-3">Continue</button>
-    </div>
-    
-    <div id="confirmation-section" class="checkout-section">
-    <h3 id="header">5.Place Order</h3>
-    <div class="confirm">
-    <div id="orderNo">
-    <h5 class="order-form">Order No.</h5>
-    </div>
-    <div id="method">
-    <h5 class="order-form">Shipping method</h5>
-    </div>
-    <div id="shipTo">
-    <h5 class="order-form">Ship To</h5>
-    </div>
-    <div id="cardInfo">
-    <h5 class="order-form">Payment</h5>
-    </div>
-    <div id="summery">
-    <h5 class="order-form">Order Summery</h5>
-    </div>
-    <div id="total">
-    <h5 class="order-form">Total price</h5>
-    </div>
-    </div>
-    <div id="term">
-    <input type="checkbox" id="term" name="term" value="term">
-    <label for="termOfSale">I have read, understood and agree to the Term of Sale</label><br>
-    <input type="checkbox" id="offer" name="offer" value="offer">
-    <label for="offer">Sign up to receive emails to be the first to know about the latest books, offers and events from BOOKS. By ticking the box and clicking 'Place Order' below upon your purchase history, website browsing and any email preferences that you provide Your personal data is cllected and handled by BOOKS. Once BOOKS has confirmed acceptance of your order, payment will be taken for the item(s) ordered.</label><br>
-    </div>
-    <button class="placeOrder"><a href="../html/successfulorder.html">Place Order</a></button>
-    </div>
-    </div>`
-    
-    $('#main-area').html(checkoutHtml);
-    $('input').prop('required', true);
+    createHTML();
+    $('input').addClass('form-control');
+    $('button').attr('type','submit');
     
     addedProducts();
     
@@ -110,22 +15,11 @@ $(function () {
     });
     
     $('.continue-2').on('click', () => {
-        $('#payment-section').addClass('flex');
-        $('#information-section').addClass('opacity');
-        $('#delivery-section').addClass('opacity');
-        setTimeout( function() {
-            window.location.hash = "#payment-section"
-        }, 100);
-        
-        
+        validateForm();
     });
     
     $('.continue-3').on('click', () => {
-        $('#confirmation-section').addClass('flex');
-        $('#payment-section').addClass('opacity');
-        setTimeout( function() {
-            window.location.hash = "#confirmation-section"
-        }, 100);
+        validateCard();
         let orderNo = Date.now();
         let name = $('#fname').val();
         let email = $('#email').val();
@@ -151,6 +45,59 @@ $(function () {
     
     
 });
+function validateForm() {
+    return $("#information-form").validate({ 
+        rules: {
+            firstname: "required",  
+            email: "required",
+            address: "required",
+            city: "required",
+            zip: "required",
+            deliveryType: "required",
+            card:"required"
+            
+        },
+        messages: {
+            firstname: "Enter First Name",
+            email: "Enter Valid Email ID",
+            address: "Enter your address",
+            city: "Enter your city",
+            zip: "Enter your zip",
+            deliveryType: "Choose a delivery",
+            card: "Choose a card"
+          
+        },
+        submitHandler: function() {
+            $('#payment-section').addClass('flex');
+            $('#information-section').addClass('opacity');
+            $('#delivery-section').addClass('opacity');
+            setTimeout( function() {
+                window.location.hash = "#payment-section"
+            }, 100);
+        }
+    });
+}
+
+function validateCard() {
+    return $("#payment-form").validate({ 
+        rules: {
+            card:"required"
+            
+        },
+        messages: {
+            card: "Choose a card"
+          
+        },
+        submitHandler: function() {
+            $('#confirmation-section').addClass('flex');
+            $('#payment-section').addClass('opacity');
+            setTimeout( function() {
+                window.location.hash = "#confirmation-section"
+            }, 100);
+        }
+    });
+}
+
 function orderForm() {
     let orderDetailList = JSON.parse(localStorage.getItem('orderDetailList'));
     let existingProducts = JSON.parse(localStorage.getItem('addedProductsList'));
@@ -264,3 +211,105 @@ function addedProducts() {
     $("#total-amount").html( totalAmount +" "+"kr");
 }
 
+function createHTML() {
+    let checkoutHtml = `
+    <div class="cart-section">
+    <h3 id="header">1. Shopping cart</h3>
+    <ol id="added-products"></ol>
+    <div class="total-amount">
+    <p>Total: </p>
+    <p id="total-amount"></p>
+    </div>
+    <button class="continue-1">Continue</button>
+    </div>
+    
+    <form action="" id="information-form">
+    <div id="information-section" class="checkout-section">
+    <h3 id="header">2.Shipping address</h3>
+    <label for="name"> Full Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Kalle Anka">
+    <label for="email"> Email</label>
+    <input type="text" id="email" name="email" placeholder="kalleAnka@example.com">
+    <label for="address"> Address</label>
+    <input type="text" id="address" name="address" placeholder="Gustavslundsvägen 151 D">
+    <label for="city"> City</label>
+    <input type="text" id="city" name="city" placeholder="Bromma">
+    <label for="zip">Zip</label>
+    <input type="text" id="zip" name="zip" placeholder="10001">
+    <label for="country">Country</label>
+    <select id="country" name="country">
+    <option value="Sweden">Sweden</option>
+    </select>
+    </div>
+    
+    <div id="delivery-section" class="checkout-section">
+    <h3 id="header">3.Shipping Method</h3>
+    <p>Delivery</p>
+    <div>
+    <input type="radio" id="standard" name="deliveryType" value="Standard delivery 4-7 Bussiness Days">
+    <label for="standard">Standard 4-7 Bussiness Days - Free </label><br>
+    <input type="radio" id="express" name="deliveryType" value="Express delivery 2-3 Bussiness Days">
+    <label for="express">Express 2-3 Bussiness Days - Free </label><br>
+    </div>
+    <button class="continue-2">Continue</button>
+    </div>
+    </form>
+    
+    <form action="" id="payment-form">
+    <div id="payment-section" class="checkout-section">  
+    <h3 id="header">4.Payment Details</h3>
+    <div>
+    <input type="radio" id="card" name="card" value="Credit Card">
+    <label for="card">Credit Card</label>
+    </div>
+    <div class="icon-container">
+    <i class="fa fa-cc-visa"></i>
+    <i class="fa fa-cc-mastercard"></i>
+    </div>
+    <label for="cardname">Name on Card</label>
+    <input type="text" id="cardname" name="cardname" placeholder="Kalle Anka">
+    <label for="cardnum">Credit card number</label>
+    <input type="text" id="cardnum" name="cardnumber" placeholder="****-****-****-****">
+    <label for="expmonth">Exp Month</label>
+    <input type="text" id="expmonth" name="expmonth" placeholder="December">
+    <label for="expyear">Exp Year</label>
+    <input type="text" id="expyear" name="expyear" placeholder="2020">
+    <label for="cvc">CVC</label>
+    <input type="text" id="cvc" name="cvc" placeholder="***">
+    <button class="continue-3">Continue</button>
+    </div>
+    </form>
+    
+    <div id="confirmation-section" class="checkout-section">
+    <h3 id="header">5.Place Order</h3>
+    <div class="confirm">
+    <div id="orderNo">
+    <h5 class="order-form">Order No.</h5>
+    </div>
+    <div id="method">
+    <h5 class="order-form">Shipping method</h5>
+    </div>
+    <div id="shipTo">
+    <h5 class="order-form">Ship To</h5>
+    </div>
+    <div id="cardInfo">
+    <h5 class="order-form">Payment</h5>
+    </div>
+    <div id="summery">
+    <h5 class="order-form">Order Summery</h5>
+    </div>
+    <div id="total">
+    <h5 class="order-form">Total price</h5>
+    </div>
+    </div>
+    <div id="term">
+    <input type="checkbox" id="term" name="term" value="term">
+    <label for="termOfSale">I have read, understood and agree to the Term of Sale</label><br>
+    <input type="checkbox" id="offer" name="offer" value="offer">
+    <label for="offer">Sign up to receive emails to be the first to know about the latest books, offers and events from BOOKS. By ticking the box and clicking 'Place Order' below upon your purchase history, website browsing and any email preferences that you provide Your personal data is cllected and handled by BOOKS. Once BOOKS has confirmed acceptance of your order, payment will be taken for the item(s) ordered.</label><br>
+    </div>
+    <button class="placeOrder"><a href="../html/successfulorder.html">Place Order</a></button>
+    </div>`
+    
+    $('#main-area').html(checkoutHtml);
+}
